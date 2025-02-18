1297ee5910c34586c995ca5f71f4a651380c2a89
Badnam xd <yadavxd746@gmail.com>
2025-02-19 00:26:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 BOT_TOKEN = getenv("BOT_TOKEN", "7833972330:AAErp7yd_GYU9UyvE84tyfvFlb0KctRe9do")
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://theriyamusic94:f67KlgTyzr3TTutn@cluster0.lym5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-OWNER_ID = int(getenv("OWNER_ID", 7668520999))
-SUPPORT_GRP = "RIYA_chat_SUPPORT"
-UPDATE_CHNL = "riya_NETWORK"
-OWNER_USERNAME = "ll_hunter_lll"
+OWNER_ID = int(getenv("OWNER_ID", 8037151602))
+SUPPORT_GRP = "friendsxzoneg"
+UPDATE_CHNL = "friendsxzoneg"
+OWNER_USERNAME = "III_SAD_HU_SUDHEER_HU_BABY_lll"
